0460af3e91b2b82f725a864c2eef7121c67758c1
aceDavon <makojidavid@gmail.com>
2022-11-22 23:18:50 +0100

    change column type as required from review
    Change column type to integer for post_counter on the users table migration

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_202628) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.string "posts_counter"
+    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
