3e6f2c73c3a8d5becb81882b192ff89a6f23d120
aceDavon <makojidavid@gmail.com>
2022-11-23 23:35:14 +0100

    Add relationships to models
    Add database relationships to models and implement counter functions

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+  after_save :update_comments_counter
+
+  belongs_to :post
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,8 @@
 class Like < ApplicationRecord
+  after_save :update_likes_counter
+  belongs_to :post
+
+  def update_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
