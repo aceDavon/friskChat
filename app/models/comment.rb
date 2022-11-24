34fecf93ff2387ac70d1eee450efa57cae14e7c5
aceDavon <makojidavid@gmail.com>
2022-11-24 07:56:49 +0100

    Add user association to like and comment
    implement the user association on the like and comment model to match the
    ERD diagram as described by the project requirement

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   after_save :update_comments_counter
 
   belongs_to :post
+  belongs_to :user
 
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ApplicationRecord
   after_save :update_likes_counter
   belongs_to :post
+  belongs_to :user
 
   def update_likes_counter
     post.update(likes_counter: post.likes.count)
