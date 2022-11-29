5a2ea04cbc71586904929971a88e5c9d9a9b9a22
aceDavon <makojidavid@gmail.com>
2022-11-29 20:34:19 +0100

    Write unit tests for controllers
    Write unit controller tests for methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    params[:id]
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /[show,' do
+  describe 'GET /show,' do
     it 'returns http success' do
-      get '/users/[show,'
+      get '/users/,'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'return homw views template' do
+      get '/'
+      expect(response).to render_template(:index)
     end
   end
 
   describe 'GET /index]' do
+    before :each do
+      get '/users/:id'
+    end
+
     it 'returns http success' do
-      get '/users/index]'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'return the show views template' do
+      expect(response).to render_template(:show)
     end
   end
 end
