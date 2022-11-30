5b29002bf23cebbd7a192a189bf0a0feda327593
aceDavon <makojidavid@gmail.com>
2022-12-01 00:43:10 +0100

    Add views interface for the post controllers
    Add the interface for te post controller and style minimally

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
   end
 
   def show
-    params[:id]
+    @post = Post.find(params[:id])
   end
 end
