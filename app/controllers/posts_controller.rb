dd7a7d6e4d42aafa4a7ca5df05e15123a345516d
aceDavon <makojidavid@gmail.com>
2022-12-02 00:05:31 +0100

    Create the form components
    Create, style and link the post forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @new_post = Post.new(post_params)
+    @new_post.author_id = current_user.id
+    @new_post.likes_counter = 0
+    @new_post.comments_counter = 0
+
+    if @new_post.save!
+      redirect_to user_posts_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
